64cbfe4eeff291dc68384112e5d17f53f7fcd090
FreitasPedro <pedrospf2018@gmail.com>
2024-07-16 17:52:30 -0300

    S7 - Sales Table

--- S7 - Learning Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ S7 - Learning Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
   templateUrl: './sales-person-list.component.html',
   styleUrl: './sales-person-list.component.css'
 })
-export class SalesPersonListComponent {
+export class SalesPersonListComponent implements OnInit {
+  
 
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup@email.com", 50000),
+    new SalesPerson("John", "Doe", "john@email.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire@email.com", 90000),
+    new SalesPerson("Mai", "Troung", "mai@email.com", 60000)
+  ];
+
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
