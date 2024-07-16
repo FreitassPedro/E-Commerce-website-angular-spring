a61497bb5d52076447dc1cda2974f8fedd301525
FreitasPedro <pedrospf2018@gmail.com>
2024-07-16 17:39:35 -0300

    S7 - Sales

---
+++ S7 - Learning Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+
+}
