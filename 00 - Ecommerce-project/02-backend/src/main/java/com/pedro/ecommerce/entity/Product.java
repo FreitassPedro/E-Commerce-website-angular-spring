0f3a2d557711d5845014a2e644364ac4e7ffe5d2
FreitasPedro <pedrospf2018@gmail.com>
2024-07-17 16:04:53 -0300

    S10: Display new Table | Image support

--- 00 - Ecommerce-project/02-backend/src/main/java/com/pedro/ecommerce/entity/Product.java
+++ 00 - Ecommerce-project/02-backend/src/main/java/com/pedro/ecommerce/entity/Product.java
@@ -41,7 +41,7 @@ public class Product {
     private boolean active;
 
     @Column(name = "units_in_stock")
-    private int units_in_stock;
+    private int unitsInStock;
 
     @Column(name = "date_created")
     @CreationTimestamp
