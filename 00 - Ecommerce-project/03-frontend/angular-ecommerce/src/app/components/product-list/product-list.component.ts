910d6b7906a8e885a300e47cd6e824575636f6e7
FreitasPedro <pedrospf2018@gmail.com>
2024-07-17 18:37:44 -0300

    S11: Grid list with Images

--- 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -4,8 +4,8 @@ import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
   //templateUrl: './product-list-table.component.html',
-  templateUrl: './product-list-table.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
