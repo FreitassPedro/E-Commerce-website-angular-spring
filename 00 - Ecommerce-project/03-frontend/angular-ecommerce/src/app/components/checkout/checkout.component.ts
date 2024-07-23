a5c308c82503203804d44235bb4be3cfcbc9432c
FreitasPedro <pedrospf2018@gmail.com>
2024-07-23 19:44:48 -0300

    S21: Validation Rules Name-Email

--- 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { OwnFormServiceService } from './../../services/own-form-service.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from '../../common/country';
 import { State } from '../../common/state';
 
@@ -31,9 +31,12 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+            [Validators.required,
+              Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+            )
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -91,14 +94,20 @@ export class CheckoutComponent implements OnInit {
   }
 
   onSubmit() {
+
     console.log('Hadling the submit button');
+
+    if (this.checkoutFormGroup.invalid) this.checkoutFormGroup.markAllAsTouched();
+    
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log(
       'Email adress is ' + this.checkoutFormGroup.get('customer')?.value.email
     );
-
-
   }
+
+  get firstName() {return this.checkoutFormGroup.get('customer.firstName');  }
+  get lastName() {return this.checkoutFormGroup.get('customer.lastName');  }
+  get email() {return this.checkoutFormGroup.get('customer.email');  }
 
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
