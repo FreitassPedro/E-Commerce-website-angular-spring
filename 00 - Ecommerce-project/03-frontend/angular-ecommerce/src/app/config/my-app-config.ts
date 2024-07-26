891f34ea40480d241e52559e79bed0b24fcef44a
FreitasPedro <pedrospf2018@gmail.com>
2024-07-26 18:48:10 -0300

    S25: Security Login Upload Dependencies

---
+++ 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  // open id connect
+  oidc: {
+    clientId: '0oail8ucufDRgt3yA5d7',
+    issuer: 'https://dev-74217793.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
