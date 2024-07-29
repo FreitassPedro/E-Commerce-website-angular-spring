b336d67569d2a68a73a8312aade0164809b06a22
FreitasPedro <pedrospf2018@gmail.com>
2024-07-28 23:15:32 -0300

    S25: NOT WORKING login/logout

--- 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -15,9 +15,22 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from '@okta/okta-angular';
+
+
+import myAppConfig from './config/my-app-config';
+import OktaAuth from '@okta/okta-auth-js';
+
+const oktaConfig = myAppConfig.oidc;
+const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
   // Se o Path bater, irá jogar para o component
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
+
   { path: 'checkout', component: CheckoutComponent },
   { path: 'cart-details', component: CartDetailsComponent },
   { path: 'search/:keyword', component: ProductListComponent },
@@ -39,6 +52,8 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -47,8 +62,9 @@ const routes: Routes = [
     HttpClientModule,
     NgbModule,
     ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth }}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ 00 - Ecommerce-project/03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,47 @@
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrl: './login-status.component.css',
+})
+export class LoginStatusComponent implements OnInit {
+
+  isAuthenticated: boolean = false;
+  userFullName: string = '';
+
+  constructor(
+    private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
+  ) {}
+
+  ngOnInit(): void {
+
+    // Subscribe to authentication state changes
+    this.oktaAuthService.authState$.subscribe(
+      (result) => {
+        this.isAuthenticated = result.isAuthenticated!;
+        this.getUserDetails();
+      }
+    );
+  }
+  getUserDetails() {
+    if (this.isAuthenticated) {
+
+      // Fetch the logged in user details (user's claims)
+      //
+      // user full name is exposed as a property name
+      this.oktaAuth.getUser().then(
+        (res) => {
+          this.userFullName = res.name as string;
+        }
+      )
+    }
+  }
+
+  logout() {
+    this.oktaAuth.signOut();
+  }
+}
